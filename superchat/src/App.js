b5cf49c9e03c39f52f5a0c11abea4eea5e92c0b2
Jordan <jordan@chude.co>
2022-08-04 19:50:16 -0400

    process env additions to app.js

--- superchat/src/App.js
+++ superchat/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import './App.css';
 
+// environment variables
+require('dotenv').config();
+
 // firebase
 import firebase from 'firebase/app'
 import 'firebase/firestore';
@@ -12,7 +15,13 @@ import { useCollectionData } from 'react-firebase-hooks/firestore'
 
 // identify project
 firebase.initializeApp({
-  // your config
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 })
 
 // references to auth and firebase sdks
