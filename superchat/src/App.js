33d67d89cd8f6a84a8dc3ce3bdc5357e22d40036
Jordan <jordan@chude.co>
2022-08-04 19:18:04 -0400

    app.js file setup prior to firebase

--- superchat/src/App.js
+++ superchat/src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+
+// firebase
+import firebase from 'firebase/app'
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// hooks
+import { useAuthState } from 'react-firebase-hooks'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+// identify project
+firebase.initializeApp({
+  // your config
+})
+
+// references to auth and firebase sdks
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
