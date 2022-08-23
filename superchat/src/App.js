9e909715def5fdaf5cdfd8c4cdce2728d867bf6e
Jordan <jordan@chude.co>
2022-08-22 21:41:43 -0400

    first component

--- superchat/src/App.js
+++ superchat/src/App.js
@@ -1,31 +1,32 @@
 import './App.css';
-import Chatroom from './components/Chat/Chatroom';
-import SignIn from './components/Welcome/SignIn';
+// import Chatroom from './components/Chat/Chatroom';
+// import SignIn from './components/Welcome/SignIn';
+import Header from './components/Chat/Header';
 
 // environment variables
 // require('dotenv').config();
 // require('dotenv-webpack').config();
 
 // firebase
-import firebase from 'firebase/compat/app'
-import 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+// import firebase from 'firebase/compat/app'
+// import 'firebase/firestore';
+// import { getAuth } from 'firebase/auth';
 
 // hooks
-import { useAuthState } from 'react-firebase-hooks/auth'
+// import { useAuthState } from 'react-firebase-hooks/auth'
 // import { SignInMethod } from 'firebase/auth';
 // import { useCollectionData } from 'react-firebase-hooks/firestore'
 
 // identify project
-firebase.initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
-})
+// firebase.initializeApp({
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+//   measurementId: process.env.REACT_APP_MEASUREMENT_ID
+// })
 
 // references to auth and firebase sdks
 // const auth = firebase.auth();
@@ -35,20 +36,22 @@ function App() {
 
 // signed, user is an object
 // signed out, user is null
-  const auth = getAuth(firebase);
-  const [user] = useAuthState(auth);
+  // const auth = getAuth(firebase);
+  // const [user] = useAuthState(auth);
 
   return (
-    <div className="App">
+    <div id="App">
       <header className="App-header">
-        
+        {/* {user ? <Header/> : null } */}
+        <Header />
       </header>
 
       <section>
-        {user ? <Chatroom/> : <SignIn />}
+        {/* {user ? <Chatroom/> : <SignIn />} */}
       </section>
     </div>
   );
 }
 
 export default App;
+
