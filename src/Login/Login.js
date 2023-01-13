2fc46c3299138e0f425b9e34bc205acf22da26c8
Jordan <jordan@chude.co>
2023-01-12 23:47:45 -0500

    login page and sign up components created

---
+++ src/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignupButton from './SignupButton';
+
+function Login(props) {
+    return (
+        <div>
+            <SignupButton />
+        </div>
+    );
+}
+
+export default Login;
---
+++ src/Login/SignupButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SignupButton(props) {
+    return (
+        <div>
+            <button>Sign In</button>
+        </div>
+    );
+}
+
+export default SignupButton;
