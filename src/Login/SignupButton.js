643c87c764b4b0199342bc35c1337f12eb635150
Jordan <jordan@chude.co>
2023-02-05 13:37:40 -0800

    sign up button component with user state

--- src/Login/SignupButton.js
+++ src/Login/SignupButton.js
@@ -5,12 +5,14 @@ const auth = getAuth();
 
 function SignupButton({email, password}) {
     const [error, setError] = useState(null);
+    const [user, setUser] = useState(null);
     
     const signIn = async (e) => {
         e.preventDefault();
 
         try {
-            await signInWithEmailAndPassword(auth, email, password);
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            setUser(userCredential.user);
         } catch (err) {
             setError(err.message)
         }
